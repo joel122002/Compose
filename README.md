2b99a4793b5dd399b8b15c3c8dba824cf1f61cd4
Joel Varghese <joel02varghese@gmail.com>
2022-07-05 17:38:59 +0530

    Colummns, Rows and Basic Sizing

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a project where I learn jetpack compose for android
 ## Commit history
 
 Commit 1: Basics of Compose
+Commit 2: Columns, Rows and basic sizing
--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +23,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         // set content sets the view for the current activity.
         setContent {
-            Greeting("there!!")
+            // If we don't place this here column and row will overlap
+            Column() {
+                // Places children one below the other. By default if you don't use column they will
+                // overlap
+                Column {
+                    Greeting("there!!")
+                    Greeting("world!!")
+                    Greeting("earth")
+                }
+                // Places children one after the other. By default if you don't use row they will
+                // overlap
+                Row {
+                    Greeting("there!!")
+                    Greeting("world!!")
+                    Greeting("earth")
+                }
+                // Parameters help give properties to a composable
+                Column (
+                    modifier = Modifier
+                        .height(90.dp)
+                        .fillMaxWidth()
+                        .background(Color.DarkGray)
+                        .border(2.dp, Color.Black),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Greeting("there!!")
+                    Greeting("world!!")
+                    Greeting("earth")
+                }
+            }
         }
     }
 }
